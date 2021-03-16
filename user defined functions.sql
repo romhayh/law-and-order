a59cb27c04e447f0822bef46dee4bb445b129941
romhayh <55787256+romhayh@users.noreply.github.com>
2021-03-16 14:11:16 +0200

    Update user defined functions.sql

--- user defined functions.sql
+++ user defined functions.sql
@@ -4,5 +4,13 @@ begin
     declare @ans int
 	set @ans = (select [city id] from cities where [city name] = @name)
     return @ans
-end
+end;
+go
 
+create function specializationidof (@area varchar(30))
+returns int as
+begin
+    declare @ans int
+	set @ans = (select s.[area] from specializations s where [area] = @area)
+    return @ans
+end
